0fd74c1e5ecbbdd127203ed03764a4dc8fc1224a
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2015-09-19 13:58:22 +0300

    Namespace the primitive types in a submodule
    It should be possible glob-import the primitive types separately from
    GType, as grust defines a safe wrapper also named GType.
    
    All types are still exported at the crate top level for glob-importing
    into FFI code.

--- gtypes.rs
+++ gtypes.rs
@@ -1,6 +1,6 @@
 // This file is part of Grust, GObject introspection bindings for Rust
 //
-// Copyright (C) 2013, 2014  Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
+// Copyright (C) 2013-2015  Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
@@ -20,8 +20,7 @@
 //! This crate provides definitions for types that are intrinsic in
 //! GObject introspection, unlike all other types that have a definition in
 //! a GIR file. All crates generated from GObject introspection data should
-//! use these shared definitions, usable in FFI declarations as well as
-//! in idiomatic Rust bindings.
+//! use these shared definitions.
 //!
 //! Some types are omitted because a suitable equivalent is readily available
 //! in Rust:
@@ -33,34 +32,62 @@
 //!    representation of C strings in idiomatic Rust bindings
 //!    needs some wrapper types which are not defined here.
 
-#![allow(non_camel_case_types)]
-
 extern crate libc;
 
 // The fundamental types are defined in Python module giscanner.ast
 
-pub type gboolean       = libc::c_int;
-pub type gchar          = libc::c_char;
-pub type guchar         = libc::c_uchar;
-pub type gshort         = libc::c_short;
-pub type gushort        = libc::c_ushort;
-pub type gint           = libc::c_int;
-pub type guint          = libc::c_uint;
-pub type glong          = libc::c_long;
-pub type gulong         = libc::c_ulong;
-pub type gsize          = libc::size_t;
-pub type gssize         = libc::ssize_t;
-pub type gintptr        = libc::intptr_t;
-pub type guintptr       = libc::uintptr_t;
-pub type gfloat         = libc::c_float;
-pub type gdouble        = libc::c_double;
-pub type gunichar       = u32;
-pub type gpointer       = *mut   libc::c_void;
-pub type gconstpointer  = *const libc::c_void;
+pub mod primitive {
 
-// GType is considered a fundamental type and used by the unqualified name
-// in GIR files, despite being also defined in both GLib and GObject.
+    //! Definitions for primitive fundamental types.
+    //!
+    //! Values of these types don't have associated semantics or state
+    //! that would necessitate some kind of wrapper types in safe Rust
+    //! bindings. Therefore, these types can be used in both
+    //! FFI declarations and idiomatic Rust bindings, unlikely to require
+    //! name disambiguation with any generated or binding types.
+    //!
+    //! The untyped pointer types are also defined in this module,
+    //! because safe Rust bindings are unlikely to need to reuse their names.
+
+    #![allow(non_camel_case_types)]
+
+    use libc;
+
+    pub type gboolean       = libc::c_int;
+    pub type gchar          = libc::c_char;
+    pub type guchar         = libc::c_uchar;
+    pub type gshort         = libc::c_short;
+    pub type gushort        = libc::c_ushort;
+    pub type gint           = libc::c_int;
+    pub type guint          = libc::c_uint;
+    pub type glong          = libc::c_long;
+    pub type gulong         = libc::c_ulong;
+    pub type gsize          = libc::size_t;
+    pub type gssize         = libc::ssize_t;
+    pub type gintptr        = libc::intptr_t;
+    pub type guintptr       = libc::uintptr_t;
+    pub type gfloat         = libc::c_float;
+    pub type gdouble        = libc::c_double;
+    pub type gunichar       = u32;
+    pub type gpointer       = *mut   libc::c_void;
+    pub type gconstpointer  = *const libc::c_void;
+
+    pub const FALSE: gboolean = 0;
+    pub const TRUE : gboolean = 1;
+}
+
+// For -sys crates only dealing with FFI types, all public names defined
+// in this crate should be conflict-free, so we make them available at the
+// crate level handy for glob-importing.
+pub use primitive::{gboolean, gchar, guchar, gshort, gushort, gint, guint};
+pub use primitive::{glong, gulong, gsize, gssize, gintptr, guintptr};
+pub use primitive::{gfloat, gdouble, gunichar, gpointer, gconstpointer};
+pub use primitive::{FALSE, TRUE};
+
+/// An integer type that designates GObject type identifiers.
+///
+/// `GType` is considered a fundamental type, regardless of being also
+/// introspected both in GLib and GObject. Unlike the primitive types,
+/// valid values of `GType` can only result from type registration,
+/// so the safe bindings are likely to provide their own `GType` wrapper.
 pub type GType = gsize;
-
-pub const FALSE: gboolean = 0;
-pub const TRUE : gboolean = 1;
